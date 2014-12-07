7ae189b664e81b5323fa03ed8e3a5478aba10e65
Laura Sanders <laura.sanders@nl.experian.com>
2014-12-07 19:02:09 +0100

    update script including download data

--- Plot4.R
+++ Plot4.R
@@ -1,7 +1,12 @@
 ##before running the script install the read.table package if you haven't done that yet.
 ## install.packages ("read.table")
 ## library(read.table)
-##set your working directory to where you've stored the data
+##set your working directory to where you've stored the data (if you skip the download of the data and the unzip command) or where you want to store the data.
+##download the data and unzip it.
+Downloadeddata<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(Downloadeddata, destfile=".\\exdata_data_household_power_consumption.zip")
+unzip("exdata_data_household_power_consumption.zip")
+
 ##read in the full data set and select only the rows of the dates of 1st and 2nd of February 2007.
 alldata<-read.table("household_power_consumption.txt", header=TRUE, sep=";",stringsAsFactors=FALSE)
 twodayfebdata<-alldata[alldata$Date %in% c("1/2/2007","2/2/2007"),]
